37acfc878e5bd12eea52dd415b2a2feb38af25ca
quduss <shuaibqudus33@gmail.com>
2024-01-20 15:34:00 +0100

    Create a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file using puppet with specific content, permissions group and owner
+
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
