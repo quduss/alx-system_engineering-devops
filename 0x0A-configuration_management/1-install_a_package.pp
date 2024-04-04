01d25a35350151e28ea5a53ae1ae35e756523d19
quduss <shuaibqudus33@gmail.com>
2024-04-04 11:29:29 +0100

    Install flask 2.1.0 from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/pup
-# Install flask version (2.1.0)
+# Install Flask using pip3
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
