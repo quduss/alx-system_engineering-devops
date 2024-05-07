aa297daa07f5b4ee62fc92820228bdedcd594a05
quduss <shuaibqudus33@gmail.com>
2024-05-07 15:58:34 +0100

    Update User-Agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "Python:RedditSubscriberCounter:v1.0 (by /u/user)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
     try:
